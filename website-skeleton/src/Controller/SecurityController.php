04584648cded102122b682ace61f89c9254cd6d5
Alex_Oclock <alex.356@hotmail.fr>
2019-10-24 11:25:07 +0200

    Add Json Web Token

--- website-skeleton/src/Controller/SecurityController.php
+++ website-skeleton/src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -32,5 +33,19 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/api/login_check", name="api_login")
+     * @return JsonResponse
+     */
+    public function api_login(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        return new Response([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
     }
 }
