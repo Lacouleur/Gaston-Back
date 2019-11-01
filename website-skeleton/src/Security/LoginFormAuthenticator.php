d5f4ed7f9bf0c2de04caba19d506a43b1a3cb8aa
Alex_Oclock <alex.356@hotmail.fr>
2019-11-01 14:58:20 +0100

    Add 403 User

--- website-skeleton/src/Security/LoginFormAuthenticator.php
+++ website-skeleton/src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('/admin'));
+        return new RedirectResponse($this->urlGenerator->generate('main'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
